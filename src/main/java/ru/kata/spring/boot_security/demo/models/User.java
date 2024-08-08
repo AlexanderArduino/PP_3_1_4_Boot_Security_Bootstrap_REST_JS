2b82d86992a44f589a9dfa3684a7db4e37829233
Александр <140169354+AlexanderArduino@users.noreply.github.com>
2024-08-08 13:42:50 +0300

    Исправление по наставлению ментора #2 3) убрал закомментированный код

--- src/main/java/ru/kata/spring/boot_security/demo/models/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/User.java
@@ -61,33 +61,17 @@ public class User implements UserDetails {
         return id;
     }
 
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-
     public String getName() {
         return name;
     }
-
-//    public void setName(String name) {
-//        this.name = name;
-//    }
 
     public String getSurname() {
         return surname;
     }
 
-//    public void setSurname(String surname) {
-//        this.surname = surname;
-//    }
-
     public String getEmail() {
         return email;
     }
-
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
 
     public void setUsername(String username) {
         this.username = username;
